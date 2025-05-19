7d9dac43ef3581e2acd6d513786c513cea2ea695
JaredNoel <noel.jared22@gmail.com>
2025-05-19 14:44:47 +0800

    Fixed server and sockets

---
+++ Menger-App/src/main/java/com/example/server/ServerStorage.java
@@ -0,0 +1,19 @@
+package com.example.server;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ServerStorage {
+    private static final String FILE_PATH = "chat_log.txt";
+    
+
+    public static synchronized void saveMessage(String message) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH, true))) {
+            writer.write(message);
+            writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
